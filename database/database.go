e1f20bcd16b7c48c030343b8fec837d3bab4ced7
CyrilleGG (wsf) <c_guiotguillin@etu-webschoolfactory.fr>
2021-10-13 17:06:22 +0200

    Changed connection config for database

--- database/database.go
+++ database/database.go
@@ -1,32 +1,30 @@
 package database
 
 import (
-	"log"
-	"os"
 	"database/sql"        // package for SQL DB operations
 	_ "github.com/lib/pq" // package for DB connection driver
-
-	"github.com/joho/godotenv" // package for interacting with .env files
+	"log"
+	"os"
 )
 
 // 		Fetching connection info for DB
-var err = godotenv.Load(".env")
-var dbHost, _ = os.LookupEnv("DB_HOST")
-var dbName, _ = os.LookupEnv("DB_DATABASE")
-var user, _ = os.LookupEnv("DB_USER")
-var pwd, _ = os.LookupEnv("DB_PASSWORD")
+//var err = godotenv.Load(".env")
+//var dbHost, _ = os.LookupEnv("DB_HOST")
+//var dbName, _ = os.LookupEnv("DB_DATABASE")
+//var user, _ = os.LookupEnv("DB_USER")
+//var pwd, _ = os.LookupEnv("DB_PASSWORD")
 
 // 		Declaring config connection for DB
-var connStr = "host=" + dbHost + " port=5432 dbname=" + dbName + " user=" + user + " password=" + pwd + " sslmode=require"
+//var connStr = "host=" + dbHost + " port=5432 dbname=" + dbName + " user=" + user + " password=" + pwd + " sslmode=require"
 
 // 		Opening connection to DB with postgres driver
 //		and check if error
 func Open() *sql.DB {
-	if err != nil {
-		log.Fatal(".env file not found...")
-		return nil
-	}
-	psql, err := sql.Open("postgres", connStr)
+	//if err != nil {
+	//	log.Fatal(".env file not found...")
+	//	return nil
+	//}
+	psql, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 		return nil
